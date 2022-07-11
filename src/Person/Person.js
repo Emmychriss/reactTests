21c6ca26adf02f3c7b7ff14c34db659378c3a32e
Emmanuel <emmychris477@gmail.com>
2022-07-11 13:04:49 +0100

    used css module style import

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,11 @@
 import React from "react";
-import "./Person.css";
+import Classes from "./Person.css";
 
 const fiveYears = 5;
 
 const person = (props) => {
-
   return (
-    <div className="Person">
+    <div className={Classes.Person}>
       <p onClick={props.clickDel}>
         I'm {props.name} and i am {props.age} years old of age. But in about{" "}
         {fiveYears} years time, I would be {+props.age + fiveYears} years old
