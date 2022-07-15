c9bb6131cf56e7ef3f768e1eed11289a15305000
Emmanuel <emmychris477@gmail.com>
2022-07-15 17:34:44 +0100

    lifecycle update hooks check

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-const persons = (props) =>
-  props.personAttr.map((personEl, i, _) => {
-    return (
-      <Person
-        clickDel={() => props.clickDel(i)}
-        name={personEl.name}
-        age={personEl.age}
-        key={personEl.id}
-        changed={(changeEvent) => props.nameChange(changeEvent, personEl.id)}
-      />
-    );
-  });
+class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log("[Persons.js] getDerivedStateFromProps");
+  //   return state;
+  // }
 
-export default persons;
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[persons.js] shouldComponentUpdate ");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[persons.js] getSnapshotBeforeUpdate ");
+    return null;
+  }
+
+  render() {
+    console.log("[persons.js] is rendering...");
+    return this.props.personAttr.map((personEl, i, _) => {
+      return (
+        <Person
+          clickDel={() => this.props.clickDel(i)}
+          name={personEl.name}
+          age={personEl.age}
+          key={personEl.id}
+          changed={(changeEvent) =>
+            this.props.nameChange(changeEvent, personEl.id)
+          }
+        />
+      );
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("[Person.js] componentDidUpdate");
+  }
+}
+
+export default Persons;
